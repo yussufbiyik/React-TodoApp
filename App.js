bdadb25b18b10b685b7750ee9dcb7f56b3b7f6e2
Yusuf Bıyık <yusufbyk3660@gmail.com>
2021-10-02 00:32:55 +0300

    ➕ Create Task component
    - Create Task component
    - Modify App.js

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Bugün Yapılacaklar</Text>
+        
+        <View style={styles.items}>
+          <Task text={'Uygulamayı tamamla ⏰'}></Task>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex           : 1,
+    backgroundColor: '#FFF6EB',
+  },
+  tasksWrapper:{
+    paddingTop       : 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle:{
+    color:'#000',
+    fontSize  : 24,
+    fontWeight: 'bold',
+  },
+  items:{
+    marginTop:10,
   },
 });
